72381a19e306bacba1a2c6ed0ebd7e7d34be35d5
kaalm1 <kaalm1@gmail.com>
2017-06-15 21:14:34 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,13 @@
 class CreateStudents < ActiveRecord::Migration
+
+  def up
+    execute "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT)"
+  end
+
+
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
